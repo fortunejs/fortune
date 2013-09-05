24a7c283447249608f351cda345eec302c27107b
Dali Zheng <dali@series.ac>
2013-09-04 23:09:08 -0700

    add multiple fortunes example, bump version to 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@
 ## Changelog
 
 **v0.0.2 (8/27/2013)**
+* Use `createConnection` instead of `connect` in mongoose, which allows for multiple database connections.
+* Expose and document the `router` namespace, which is actually an instance of `express`.
+* Remove fields from the response that are not specified in the schema.
+
+**v0.0.2 (8/27/2013)**
 * Fix associations in MongoDB adapter.
 * Improve request validation.
 
---
+++ examples/multiple_fortunes.js
@@ -0,0 +1,38 @@
+var fortune = require('../lib/fortune');
+
+/**
+ * Example demonstrating two different databases being
+ * exposed through one API. Note that each instance is
+ * not automatically aware of any other instances, so
+ * you will have to solve any problems that arise from
+ * that yourself.
+ */
+var container = fortune.express()
+  , port = process.argv[2] || 1337;
+
+var peopleAPI = fortune({
+  db: 'people'
+})
+.resource('person', {
+  name: String,
+  age: Number
+});
+
+var animalsAPI = fortune({
+  db: 'animals'
+})
+.resource('animal', {
+  name: String,
+  type: String,
+  age: Number
+});
+
+container
+  .use(peopleAPI.router)
+  .use(animalsAPI.router)
+  .get('/', function(req, res) {
+    res.send('Hello, you have reached the zoo API.');
+  })
+  .listen(port);
+
+console.log('Listening on port ' + port + '...');
