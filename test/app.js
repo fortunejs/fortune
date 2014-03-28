085bdc2962347cfcf14c1f2164b20d2b21b79eb9
KKudryavtsev <konstantin.k.ed@gmail.com>
2014-03-28 11:48:40 +0000

    can set arbitrary fields as PKs now

--- test/app.js
+++ test/app.js
@@ -6,16 +6,23 @@ function createApp(adapter, options, port) {
   .resource('person', {
     name: String,
     appearances: Number,
+    email: String,
     pets: ['pet'],
-    soulmate: {ref: 'person', inverse: 'soulmate'},
-    lovers: [{ref: 'person', inverse: 'lovers'}]
-  })
+    soulmate: {ref: 'person', inverse: 'soulmate', pkType: String},
+    lovers: [{ref: 'person', inverse: 'lovers', pkType: String}]
+  }, {model: {pk:"email"}})
 
   .resource('pet', {
     name: String,
     appearances: Number,
-    owner: 'person'
+    owner: {ref:'person', pkType: String}
   })
+
+  .resource('car', {
+    licenseNumber: String,
+    model: String,
+    owner: {ref:'person', pkType: String}
+  },{ model: { pk: "licenseNumber" } })
 
   .after('person', function() {
     this.nickname = 'Super ' + this.name;
