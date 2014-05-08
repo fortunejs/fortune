ee52ef14cb110313ce20aaca919632af0ac59700
Craig Edmunds <craig.edmunds@gmail.com>
2014-05-08 19:24:28 +0100

    Added relationships example

---
+++ examples/relationships.js
@@ -0,0 +1,52 @@
+var fortune = require('../lib/fortune');
+
+/**
+ * Example application. It implements an imageboard with boards & posts.
+ * Note that this implementation does not include access control,
+ * so it's more like a wiki but without the moderation.
+ */
+fortune({
+
+  adapter: "mongodb",
+  db: 'userAddress'
+
+})
+
+/*!
+ * Define resources
+ */
+.resource("user", {
+  title : String,
+  firstName : String,
+  lastName : String,
+  role : String,
+  email : String,
+  nationality: String,
+  languageCode: String,
+  addresses: [{ref: "address", inverse: "user"}],
+  flights: [{ref: "flight", inverse: "flights"}]
+})
+
+.resource("address", {
+  type: String,
+  addressLine1: String,
+  addressLine2: String,
+  addressLine3: String,
+  addressLine4: String,
+  city: String,
+  region: String,
+  postCode: String,
+  country: String,
+  dateDeleted: Date,
+  user: {ref: "user", inverse: "addresses"}
+})
+
+.resource("flight", {
+  flightNumber: String,
+  users: [{ref: "user", inverse: "users"}]
+})
+
+/*!
+ * Start the API
+ */
+.listen(process.argv[2] || 1337);
