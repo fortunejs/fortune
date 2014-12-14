8d203077dcb2d65cf148f27414888dc0635e0b0e
kristof sajdak <kristof.sajdak@gmail.com>
2014-12-14 22:26:33 +0100

    refactored a bit more so that the before callback is less verbose

--- test/app.js
+++ test/app.js
@@ -3,6 +3,7 @@ var RSVP = require('rsvp');
 
 function createApp(options) {
     var fortuneApp = fortune(options)
+
             .resource('person', {
                 name: String,
                 appearances: Number,
@@ -10,33 +11,31 @@ function createApp(options) {
                 soulmate: {ref: 'person', inverse: 'soulmate'},
                 lovers: [{ref: 'person', inverse: 'lovers'}]
             })
+
             .resource('pet', {
                 name: String,
                 appearances: Number,
                 owner: 'person'
             })
+
             .resource('foobar', {
                 foo: String
             })
-            .transform(
+            .before(
             function (req, res) {
                 var foobar = this;
-                return new RSVP.Promise(function(resolve, reject) {
-                    if (foobar.foo && foobar.foo !== 'bar') {
-                        var error = new Error();
-                        error.problem = {
-                            httpStatus: 400,
-                            title: 'foobar error',
-                            detail: 'Foo was not bar'
-                        };
-                        reject(error);
-                    }
-                    else {
-                        resolve(foobar);
-                    }
-                });
-            },
-            function () {
+                if (foobar.foo && foobar.foo !== 'bar') {
+                    var error = new Error();
+                    error.problem = {
+                        httpStatus: 400,
+                        title: 'foobar error',
+                        detail: 'Foo was not bar'
+                    };
+                    throw error;
+                }
+                else {
+                    return foobar;
+                }
             })
         ;
 
