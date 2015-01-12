974ec27a3da9bfa590bdd5d620e00f3deb0aff7f
kristof sajdak <kristof.sajdak@gmail.com>
2015-01-12 02:08:40 +0100

    sleeping in between callbacks to prevent reprocessing same oplog.rs entries across tests added process.env.TRAVIS conditional to exclude events-reader.js tests. this env doesn't have an oplog exposed

--- test/fortune/events-reader.js
+++ test/fortune/events-reader.js
@@ -27,199 +27,206 @@ var fortune = require('../../lib/fortune');
 var createCanAlarmResponsePromise;
 var createCanAlaramResponseDfd;
 
-describe('onChange', function () {
+// travis mongodb service doesn't have oplog.rs exposed
+// so these tests will not work there
+if (!process.env.TRAVIS) {
+    describe('onChange', function () {
 
-    before(function (done) {
+        before(function (done) {
 
-        var that = this;
-        that.timeout(100000);
+            var that = this;
+            that.timeout(100000);
 
-        var options = {
-            adapter: 'mongodb',
-            connectionString: process.argv[2] || process.env.MONGODB_URL || "‌mongodb://127.0.0.1:27017/test",
-            db: 'test',
-            inflect: true
-        };
+            var options = {
+                adapter: 'mongodb',
+                connectionString: process.argv[2] || process.env.MONGODB_URL || "‌mongodb://127.0.0.1:27017/test",
+                db: 'test',
+                inflect: true
+            };
 
-        that.fortuneApp = fortune(options)
+            that.fortuneApp = fortune(options)
 
-            .resource('alarmDetail', {
-                comparator: Number,
-                valueThreshold: Number,
-                timeThreshold: Number,
-                createDate: Date,
-                createdBy: String,
-                alarmId: String,
-                prevAlarmId: String
-            })
-            .onChange({insert: createTelemetryAlarm});
+                .resource('alarmDetail', {
+                    comparator: Number,
+                    valueThreshold: Number,
+                    timeThreshold: Number,
+                    createDate: Date,
+                    createdBy: String,
+                    alarmId: String,
+                    prevAlarmId: String
+                })
+                .onChange({insert: createTelemetryAlarm});
 
-        function createTelemetryAlarm(id) {
-            console.log('handler triggered : alarmDetail ' + id);
-            return that.fortuneApp.adapter.find('alarmDetail', id.toString())
-                .then(function (alarmDetail) {
-                    rp.debug = true;
-                    return rp(
-                        {
-                            resolveWithFullResponse: true,
-                            uri: telemetryBaseUri + '/canAlarms',
-                            method: 'POST',
-                            json: {
-                                canAlarms: [
-                                    {
-                                        id: alarmDetail.alarmId,
-                                        comparator: alarmDetail.comparator,
-                                        valueThreshold: alarmDetail.valueThreshold,
-                                        timeThreshold: alarmDetail.timeThreshold
-                                    }
-                                ]
-                            }
-                        })
-                        // then catch handlers below are added to be able to assert results
-                        // this is not common for production code
-                        .then(function (response) {
-                            console.log('resolved');
-                            createCanAlaramResponseDfd.resolve(response);
-                        })
-                });
-        }
-
-        that.fortuneApp
-            .onRouteCreated('alarmDetail')
-            .then(function () {
-                // do once
-                that.fortuneApp.listen(8001);
-                done();
-            })
-            .catch(function (err) {
-                done(err);
-            });
-
-
-    });
-
-    function test(done, mockCanAlarms) {
-        var that = this;
-        that.timeout(100000);
-        var chaiExpress = chai.request(that.fortuneApp.router);
-
-        mockCanAlarms();
-
-        chaiExpress.post('/alarmDetails')
-            .send(
-            {
-                alarmDetails: [
-                    {
-                        comparator: 1,
-                        valueThreshold: 100,
-                        timeThreshold: 20,
-                        createDate: new Date(),
-                        createdBy: 'kristof'
-                    }
-                ]
-            })
-            .then(function (res) {
-                expect(res).to.have.status(201);
-                console.log(res.body);
-                createCanAlarmResponsePromise
-                    .then(function (createCanAlarmResponse) {
-                        expect(createCanAlarmResponse).to.have.status(201);
-                        done();
-                    })
-                    .catch(function (err) {
-                        console.trace(err);
-                        done(err)
+            function createTelemetryAlarm(id) {
+                console.log('handler triggered : alarmDetail ' + id);
+                return that.fortuneApp.adapter.find('alarmDetail', id.toString())
+                    .then(function (alarmDetail) {
+                        rp.debug = true;
+                        return rp(
+                            {
+                                resolveWithFullResponse: true,
+                                uri: telemetryBaseUri + '/canAlarms',
+                                method: 'POST',
+                                json: {
+                                    canAlarms: [
+                                        {
+                                            id: alarmDetail.alarmId,
+                                            comparator: alarmDetail.comparator,
+                                            valueThreshold: alarmDetail.valueThreshold,
+                                            timeThreshold: alarmDetail.timeThreshold
+                                        }
+                                    ]
+                                }
+                            })
+                            // then catch handlers below are added to be able to assert results
+                            // this is not common for production code
+                            .then(function (response) {
+                                console.log('resolved');
+                                createCanAlaramResponseDfd.resolve(response);
+                            })
                     });
-            })
-            .catch(function (err) {
-                done(err);
-            });
-    }
+            }
 
-
-    describe('Scenario: insert resources in 2 different APIs with eventual consistency', function () {
-        describe('Given no pre-existing alarmDetails', function () {
-            describe('When a new alarmDetail is posted to the alarmDetails resource, ' +
-                'an onchange handler is defined which calls out to the canAlarms resource, ', function () {
-
-                beforeEach(function(done) {
-                    var that = this;
-                    that.timeout(100000);
-
-
-                    createCanAlaramResponseDfd = RSVP.defer();
-                    createCanAlarmResponsePromise = createCanAlaramResponseDfd.promise;
-
-                    console.log('drop database');
-                    that.fortuneApp.adapter.db.db.dropDatabase();
-
-                    return require('../../lib/events-reader')(that.fortuneApp, process.env.OPLOG_MONGODB_URL || process.argv[3])
-                        .then(function (eventsReader) {
-                            that.eventsReader = eventsReader;
-
-                            function tailAndDone() {
-                                that.eventsReader.tail()
-                                    .then(function () {
-                                        done();
-                                    }); // no need to add a catch here, events-reader exits in case of an error
-                            }
-
-                            var now = BSON.Timestamp(0, (new Date() / 1000));
-                            console.log('creating checkpoint with ts ' + now.getHighBits());
-                            return that.fortuneApp.adapter.create('checkpoint', {ts: now})
-                                .then(function () {
-                                    setTimeout(tailAndDone, 500);
-                                });
-
-                        })
-                        .catch(function (err) {
-                            done(err);
-                        });
+            that.fortuneApp
+                .onRouteCreated('alarmDetail')
+                .then(function () {
+                    // do once
+                    that.fortuneApp.listen(8001);
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
                 });
 
-                afterEach(function() {
-                    this.eventsReader.stop()
-                        .then(function() {
+
+        });
+
+        function test(done, mockCanAlarms) {
+            var that = this;
+            that.timeout(100000);
+            var chaiExpress = chai.request(that.fortuneApp.router);
+
+            mockCanAlarms();
+
+            chaiExpress.post('/alarmDetails')
+                .send(
+                {
+                    alarmDetails: [
+                        {
+                            comparator: 1,
+                            valueThreshold: 100,
+                            timeThreshold: 20,
+                            createDate: new Date(),
+                            createdBy: 'kristof'
+                        }
+                    ]
+                })
+                .then(function (res) {
+                    expect(res).to.have.status(201);
+                    console.log(res.body);
+                    createCanAlarmResponsePromise
+                        .then(function (createCanAlarmResponse) {
+                            expect(createCanAlarmResponse).to.have.status(201);
                             done();
                         })
                         .catch(function (err) {
-                            done(err);
+                            console.trace(err);
+                            done(err)
                         });
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        }
+
+
+        describe('Scenario: insert resources in 2 different APIs with eventual consistency', function () {
+            describe('Given no pre-existing alarmDetails', function () {
+                describe('When a new alarmDetail is posted to the alarmDetails resource, ' +
+                    'an onchange handler is defined which calls out to the canAlarms resource, ', function () {
+
+                    beforeEach(function (done) {
+                        var that = this;
+                        that.timeout(100000);
+
+
+                        createCanAlaramResponseDfd = RSVP.defer();
+                        createCanAlarmResponsePromise = createCanAlaramResponseDfd.promise;
+
+                        console.log('drop database');
+                        that.fortuneApp.adapter.db.db.dropDatabase();
+
+                        return require('../../lib/events-reader')(that.fortuneApp, process.env.OPLOG_MONGODB_URL || process.argv[3])
+                            .then(function (eventsReader) {
+                                that.eventsReader = eventsReader;
+
+                                function tailAndDone() {
+                                    that.eventsReader.tail()
+                                        .then(function () {
+                                            done();
+                                        }); // no need to add a catch here, events-reader exits in case of an error
+                                }
+
+                                // sleep 1000 to prevent we are reprocessing oplgo entries from the previous test
+                                // precision for an oplog ts is 1s
+                                require('sleep').sleep(1);
+                                var now = BSON.Timestamp(0, (new Date() / 1000));
+                                console.log('creating checkpoint with ts ' + now.getHighBits());
+                                return that.fortuneApp.adapter.create('checkpoint', {ts: now})
+                                    .then(function () {
+                                        setTimeout(tailAndDone, 500);
+                                    });
+
+                            })
+                            .catch(function (err) {
+                                done(err);
+                            });
+                    });
+
+                    afterEach(function () {
+                        this.eventsReader.stop()
+                            .then(function () {
+                                done();
+                            })
+                            .catch(function (err) {
+                                done(err);
+                            });
+                    });
+
+
+                        it('and that resource responds with a 201 created' +
+                            'Then the onChange handler should complete successfully', function (done) {
+                            test.call(this, done, function () {
+
+                                nock(telemetryBaseUri, {allowUnmocked: true})
+                                    .post('/canAlarms')
+                                    .reply(201, function (uri, requestBody) {
+                                        return requestBody;
+                                    });
+                            });
+                        });
+
+
+                        it('and that resource responds with a 500 the first time, a 201 created the second time' +
+                            'Then the onChange handler should complete successfully', function (done) {
+                            test.call(this, done, function () {
+                                nock(telemetryBaseUri, {allowUnmocked: true})
+                                    .post('/canAlarms')
+                                    .reply(500)
+                                    .post('/canAlarms')
+                                    .reply(201, function (uri, requestBody) {
+                                        return requestBody;
+                                    });
+                            });
+                        });
+
                 });
 
-
-                    it('and that resource responds with a 201 created' +
-                        'Then the onChange handler should complete successfully', function (done) {
-                        test.call(this, done, function () {
-
-                            nock(telemetryBaseUri, {allowUnmocked: true})
-                                .post('/canAlarms')
-                                .reply(201, function (uri, requestBody) {
-                                    return requestBody;
-                                });
-                        });
-                    });
-
-
-                    it('and that resource responds with a 500 the first time, a 201 created the second time' +
-                        'Then the onChange handler should complete successfully', function (done) {
-                        test.call(this, done, function () {
-                            nock(telemetryBaseUri, {allowUnmocked: true})
-                                .post('/canAlarms')
-                                .reply(500)
-                                .post('/canAlarms')
-                                .reply(201, function (uri, requestBody) {
-                                    return requestBody;
-                                });
-                        });
-                    });
-
             });
-
         });
+
+
     });
-
-
-});
+}
 
 
