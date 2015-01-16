83a227c8ed569b52776269919d76d872e3d92cc2
kristof sajdak <kristof.sajdak@gmail.com>
2015-01-17 00:57:38 +0100

    added a test for skip

--- test/fortune/events-reader.js
+++ test/fortune/events-reader.js
@@ -38,81 +38,200 @@ var createReportResponseDfd;
 
 describe('onChange callback, event capture and at-least-once delivery semantics', function () {
 
-    before(function (done) {
+    describe('Given a post on a very controversial topic, ' +
+    'and a new comment is posted or updated with content which contains profanity, ' +
+    'the comment is reported as abusive to another API. ', function () {
 
-        var that = this;
-        that.timeout(100000);
+        before(function (done) {
 
-        var options = {
-            adapter: 'mongodb',
-            connectionString: process.argv[2] || process.env.MONGODB_URL || "‌mongodb://127.0.0.1:27017/test",
-            db: 'test',
-            inflect: true
-        };
+            var that = this;
+            that.timeout(100000);
 
-        that.fortuneApp =
-            fortune(options)
-                .resource('post', {
-                    title: String
+            var options = {
+                adapter: 'mongodb',
+                connectionString: process.argv[2] || process.env.MONGODB_URL || "‌mongodb://127.0.0.1:27017/test",
+                db: 'test',
+                inflect: true
+            };
+
+            that.fortuneApp =
+                fortune(options)
+                    .resource('post', {
+                        title: String
+                    })
+                    .onChange({
+                        delete: function () {
+                            console.log('deleted a post')
+                        }
+                    })
+                    .resource('comment', {
+                        body: String,
+                        post: 'post'
+                    })
+                    .onChange({insert: reportAbusiveLanguage, update: reportAbusiveLanguage});
+
+            that.chaiExpress = chai.request(that.fortuneApp.router);
+
+            var profanity = require('profanity-util');
+
+            function reportAbusiveLanguage(id) {
+                return that.fortuneApp.adapter.find('comment', id.toString())
+                    .then(function (comment) {
+                        var check = profanity.check(comment);
+                        if (!!check && check.length > 0) {
+                            return $http(
+                                {
+                                    uri: reportAPI_baseUri + '/reports',
+                                    method: 'POST',
+                                    json: {
+                                        reports: [
+                                            {
+                                                content: comment.body
+                                            }
+                                        ]
+                                    }
+                                })
+                                // then catch handlers below are added to be able to assert results
+                                // this is not common for production code
+                                .spread(function (response, reports) {
+                                    createReportResponseDfd.resolve(response);
+                                })
+                        } else {
+                            return false;
+                        }
+                    });
+            }
+
+            that.fortuneApp
+                .onRouteCreated('comment')
+                .then(function () {
+                    // do once
+                    that.fortuneApp.listen(8001);
+                    done();
                 })
-                .resource('comment', {
-                    body: String,
-                    post: 'post'
-                })
-                .onChange({insert: reportAbusiveLanguage, update: reportAbusiveLanguage});
-
-        var profanity = require('profanity-util');
-
-        function reportAbusiveLanguage(id) {
-            return that.fortuneApp.adapter.find('comment', id.toString())
-                .then(function (comment) {
-                    var check = profanity.check(comment);
-                    if (!!check && check.length > 0) {
-                        return $http(
-                            {
-                                uri: reportAPI_baseUri + '/reports',
-                                method: 'POST',
-                                json: {
-                                    reports: [
-                                        {
-                                            content: comment.body
-                                        }
-                                    ]
-                                }
-                            })
-                            // then catch handlers below are added to be able to assert results
-                            // this is not common for production code
-                            .spread(function (response, reports) {
-                                createReportResponseDfd.resolve(response);
-                            })
-                    } else {
-                        return false;
-                    }
+                .catch(function (err) {
+                    done(err);
                 });
-        }
 
-        that.fortuneApp
-            .onRouteCreated('comment')
-            .then(function () {
-                // do once
-                that.fortuneApp.listen(8001);
-                done();
-            })
-            .catch(function (err) {
-                done(err);
+
+        });
+
+        beforeEach(function (done) {
+            var that = this;
+            that.timeout(100000);
+
+            createReportResponseDfd = RSVP.defer();
+            createReportPromise = createReportResponseDfd.promise;
+
+            console.log('drop database');
+            that.fortuneApp.adapter.db.db.dropDatabase();
+
+            that.checkpointCreated = that.fortuneApp.eventsReader(process.env.OPLOG_MONGODB_URL || process.argv[3])
+                .then(function (EventsReader) {
+
+                    that.eventsReader = new EventsReader();
+
+                    // sleep 1000 to prevent we are reprocessing oplgo entries from the previous test
+                    // precision for an oplog ts is 1s
+                    require('sleep').sleep(1);
+                    var now = BSON.Timestamp(0, (new Date() / 1000));
+
+                    console.log('creating checkpoint with ts ' + now.getHighBits());
+                    return that.fortuneApp.adapter.create('checkpoint', {ts: now}).then(function () {
+                        return done();
+                    });
+
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        afterEach(function () {
+            this.eventsReader.stop()
+                .then(function () {
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
+        describe('When a new post is added', function () {
+            it('should skip as there is only a change handler fn defined on delete', function (done) {
+
+                var that = this;
+                that.timeout(100000);
+
+                that.eventsReader.skip = function (dfd, doc) {
+                    if (doc.ns === 'test.posts') {
+                        dfd.resolve();
+                        done();
+                    }
+                };
+
+                that.chaiExpress.post('/posts')
+                    .send({
+                        posts: [{
+                            title: "a simple topic"
+                        }]
+                    })
+                    .catch(function (err) {
+                        console.trace(err);
+                        done(err);
+                    });
+
+                that.checkpointCreated.then(function () {
+                    setTimeout(that.eventsReader.tail.bind(that.eventsReader), 500);
+                });
+
             });
+        });
 
+
+        describe('When that abuse report API resource responds with a 201 created', function () {
+            it('Then the event is considered as handled and should complete successfully with an updated checkpoint', function (done) {
+                test.call(this, done, function () {
+
+                    nock(reportAPI_baseUri, {allowUnmocked: true})
+                        .post('/reports')
+                        .reply(201, function (uri, requestBody) {
+                            return requestBody;
+                        });
+                    //todo add verify checkpoint
+                });
+            });
+        });
+
+
+        describe('When that abuse report API resource responds the first time with a 500', function () {
+            it('Then the event is retried and should complete successfully if the abuse report API responds with a 201 this time', function (done) {
+                test.call(this, done, function () {
+                    nock(reportAPI_baseUri, {allowUnmocked: true})
+                        .post('/reports')
+                        .reply(500)
+                        .post('/reports')
+                        .reply(201, function (uri, requestBody) {
+                            return requestBody;
+                        });
+                    //todo add verify checkpoint
+                });
+            });
+        });
 
     });
 
-    function test(done, mockBars) {
+    function test(done, mockReports) {
         var that = this;
         that.timeout(100000);
-        var chaiExpress = chai.request(that.fortuneApp.router);
 
-        mockBars();
+        mockReports();
 
-        chaiExpress.post('/posts')
+        that.checkpointCreated.then(function () {
+            setTimeout(that.eventsReader.tail.bind(that.eventsReader), 500);
+        });
+
+        that.chaiExpress.post('/posts')
             .send({
                 posts: [{
                     title: "a very controversial topic"
@@ -120,7 +239,7 @@ describe('onChange callback, event capture and at-least-once delivery semantics'
             })
             .then(function (postResponse) {
                 expect(postResponse).to.have.status(201);
-                return chaiExpress.post('/comments')
+                return that.chaiExpress.post('/comments')
                     .send(
                     {
                         comments: [
@@ -147,92 +266,6 @@ describe('onChange callback, event capture and at-least-once delivery semantics'
                 done(err);
             });
     }
-
-
-    describe('Given a post on a very controversial topic, ' +
-    'and a new comment is posted or updated with content which contains profanity, ' +
-    'the comment is reported as abusive to another API. ', function () {
-
-        beforeEach(function (done) {
-            var that = this;
-            that.timeout(100000);
-
-
-            createReportResponseDfd = RSVP.defer();
-            createReportPromise = createReportResponseDfd.promise;
-
-            console.log('drop database');
-            that.fortuneApp.adapter.db.db.dropDatabase();
-
-            return that.fortuneApp.eventsReader(process.env.OPLOG_MONGODB_URL || process.argv[3])
-                .then(function (EventsReader) {
-                    that.eventsReader = new EventsReader();
-
-                    function tailAndDone() {
-                        that.eventsReader.tail()
-                            .then(function () {
-                                done();
-                            }); // no need to add a catch here, events-reader exits in case of an error
-                    }
-
-                    // sleep 1000 to prevent we are reprocessing oplgo entries from the previous test
-                    // precision for an oplog ts is 1s
-                    require('sleep').sleep(1);
-                    var now = BSON.Timestamp(0, (new Date() / 1000));
-
-                    console.log('creating checkpoint with ts ' + now.getHighBits());
-                    return that.fortuneApp.adapter.create('checkpoint', {ts: now})
-                        .then(function () {
-                            setTimeout(tailAndDone, 500);
-                        });
-
-                })
-                .catch(function (err) {
-                    done(err);
-                });
-        });
-
-        afterEach(function () {
-            this.eventsReader.stop()
-                .then(function () {
-                    done();
-                })
-                .catch(function (err) {
-                    done(err);
-                });
-        });
-
-
-        it('When that abuse report API resource responds with a 201 created' +
-        'Then the event is considered as handled and should complete successfully with an updated checkpoint', function (done) {
-            test.call(this, done, function () {
-
-                nock(reportAPI_baseUri, {allowUnmocked: true})
-                    .post('/reports')
-                    .reply(201, function (uri, requestBody) {
-                        return requestBody;
-                    });
-                //todo add verify checkpoint
-            });
-        });
-
-
-        it('When that abuse report API resource responds the first time with a 500' +
-        'Then the event is retried and should complete successfully if the abuse report API responds with a 201 this time', function (done) {
-            test.call(this, done, function () {
-                nock(reportAPI_baseUri, {allowUnmocked: true})
-                    .post('/reports')
-                    .reply(500)
-                    .post('/reports')
-                    .reply(201, function (uri, requestBody) {
-                        return requestBody;
-                    });
-                //todo add verify checkpoint
-            });
-        });
-
-    });
-
 });
 
 
