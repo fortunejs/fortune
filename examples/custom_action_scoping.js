0fcc2de610d3f49d32fe32bdc6afc4a1fff307d9
Craig Edmunds <craig.edmunds@gmail.com>
2015-01-21 16:44:53 +0000

    Added scoping action example

---
+++ examples/custom_action_scoping.js
@@ -0,0 +1,50 @@
+var fortune = require('../lib/fortune');
+
+/**
+ * Example application. It implements an imageboard with boards & posts.
+ * Note that this implementation does not include access control,
+ * so it's more like a wiki but without the moderation.
+ */
+fortune({
+
+  adapter: "mongodb",
+  db: 'relationships_minimal'
+
+})
+
+
+var adminUsers = {
+  name: 'admin-users',
+  method: 'GET',
+  type: 'scoping',
+  filter: { userType : "admin" },
+  config: {
+    configHeader: 'set from init function'
+  }
+};
+
+/*!
+ * Define resources
+ */
+.resource("user", {
+	userType : String,
+	title : String,
+  firstName : String,
+  lastName : String,
+  email: String,
+  addresses: [{ref: "address", inverse: "user"}]
+}, {
+  model: {pk:"email"},
+  actions: {
+    'admin-users': adminUsers
+  }
+})
+
+.resource("address", {
+  user: {ref: "user", inverse: "addresses", pkType: String}
+})
+
+/*!
+ * Start the API
+ */
+.listen(process.argv[2] || 1337);
