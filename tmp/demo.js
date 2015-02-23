c89ea6e8467a15173229b7c1c105f7922e6739c1
Dali Zheng <d@liwa.li>
2015-02-22 16:43:13 -0800

    fix requestListener

--- tmp/demo.js
+++ tmp/demo.js
@@ -1,6 +1,6 @@
 import http from 'http';
 import chalk from 'chalk';
-import request from 'request';
+import fetch from 'node-fetch';
 import Fortune from '../lib';
 
 const PORT = 1337;
@@ -34,31 +34,15 @@ App.init().then(() => {
   http.createServer(Fortune.Net.requestListener.bind(App)).listen(PORT);
   console.log(chalk.magenta(`Listening on port ${chalk.bold(PORT)}...`));
 
-  App.request({
-    action: 'create',
-    type: 'animal',
-    payload: [{
-      _id: 'foo'
-    }],
-    ids: [2],
-    relatedField: 'owner',
-    include: [['owner'], ['pets']],
-    serializerInput: 'application/vnd.api+json',
-    serializerOutput: 'application/vnd.api+json'
-  }).then((result) => {
-    //console.log(JSON.stringify(result, null, 2));
-    console.log(result);
-  }, (error) => {
-    console.log(error);
-  });
-
-  /*request({
-    uri: `http:${'//'}localhost:${PORT}/users/1,2,3`,
-    method: 'get',
+  fetch(`http:${'//'}localhost:${PORT}/animals/5,6/owner`, {
+    method: 'POST',
     headers: {
-      Accepts: 'application/*'
-    }
-  }, (error, response, body) => {
-    console.log(body);
-  });*/
+      'Accept': 'application/*',
+      'Content-Type': 'application/vnd.api+json'
+    },
+    body: JSON.stringify({data: [{
+      _id: 'foo'
+    }]})
+  }).then(response => response.json())
+  .then(json => console.log(JSON.stringify(json, null, 2)));
 });
