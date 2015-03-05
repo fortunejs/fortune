a03adb352dfbd74051c7391ce42a38864431f3df
Mehdi Avdi <mehdi.avdi@gmail.com>
2015-03-05 23:59:29 +0000

    EventSource now sends down properly deserialized object

--- test/harvester/event-source.js
+++ test/harvester/event-source.js
@@ -3,7 +3,8 @@ var harvester = require('../../lib/harvester');
 var baseUrl = 'http://localhost:' + 8001;
 var chai = require('chai');
 var expect = chai.expect;
-
+var ess = require('event-source-stream');
+var _ = require('lodash');
 /*$http.debug = true;*/
 //$http.request = require('request-debug')($http.request);
 
@@ -13,7 +14,6 @@ describe('onChange callback, event capture and at-least-once delivery semantics'
     'and a new comment is posted or updated with content which contains profanity, ' +
     'the comment is reported as abusive to another API. ', function () {
 
-      console.log('testing')
         before(function (done) {
 
             var that = this;
@@ -41,25 +41,48 @@ describe('onChange callback, event capture and at-least-once delivery semantics'
             it('Then a "change" route should be added to that resource', function (done) {
               var that = this;
               that.timeout(100000);
-              var ess = require('event-source-stream');
               var dataReceived; 
               ess(baseUrl + '/posts/changes')
               .on('data', function(data) {
                 if (dataReceived) return;
                 dataReceived = true;
-                console.log('received event:', data)
                 expect(data).to.exist;
                 done();
               });
 
               return $http({uri: baseUrl + '/posts', method: 'POST',json: {
-                          posts: [
-                              {
-                                  title : 'test title'
-                              }
-                          ]
-                      }});
+                      posts: [
+                          {
+                              title : 'test title'
+                          }
+                      ]
+                  }});
             });
-        })
+        });
     });
+
+    describe('When I post to the newly created resource', function () {
+        it('Then I should receive a change event with data equal to what I posted', function (done) {
+          var that = this;
+          that.timeout(100000);
+          var dataReceived; 
+          ess(baseUrl + '/posts/changes')
+          .on('data', function(data) {
+            if (dataReceived) return;
+            dataReceived = true;
+            var data = JSON.parse(data);
+            expect(_.omit(data, 'id')).to.deep.equal({title : 'test title'});
+            done();
+          });
+
+          return $http({uri: baseUrl + '/posts', method: 'POST',json: {
+                  posts: [
+                      {
+                          title : 'test title'
+                      }
+                  ]
+              }});
+        });
+    });
+
 });
