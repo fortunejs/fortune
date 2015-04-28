29ab9e1245349708d3140e7aead70e1c8739afc1
Paulius <paulius.didziulis@gmail.com>
2015-04-28 18:41:39 +0100

    improved validation tests

--- test/fortune/validation.js
+++ test/fortune/validation.js
@@ -40,7 +40,7 @@ module.exports = function(options) {
 	    done();
 	  });
 
-	  it('should prevent from creating this resource', function(done) {
+	  it('should prevent creation of an invalid resource via POST', function(done) {
 	    request(baseUrl)
 	      .post('/people')
 	      .expect(403)
@@ -52,7 +52,7 @@ module.exports = function(options) {
 	      });
 	  });
 
-	  it('should allow to create this resource', function(done) {
+	  it('should allow creation of a valid resource via POST', function(done) {
 	    request(baseUrl)
 	      .post('/people')
 	      .expect(201)
@@ -64,7 +64,7 @@ module.exports = function(options) {
 	      });
 	  });
 
-	  it('should prevent from updating this resource', function(done) {
+	  it('should prevent PATCH with an invalid value', function(done) {
 	    request(baseUrl)
 	      .patch('/people/' + ids.people[0])
 	      .expect(403)
@@ -76,7 +76,7 @@ module.exports = function(options) {
 	      });
 	  });
 
-	  it('should allow to update this resource', function(done) {
+	  it('should allow PATCH with a valid value', function(done) {
 	    request(baseUrl)
 	      .patch('/people/' + ids.people[0])
 	      .expect(200)
@@ -84,6 +84,30 @@ module.exports = function(options) {
 	      .send(JSON.stringify([{ "path" : "/people/0/name", "op": "replace", "value": "Chris" }]))
 	      .end(function(error, response) {
 	        if(error) return done(error);
+	        done();
+	      });
+	  });
+
+		it('should receive an error response in correct format when POSTing', function(done) {
+	    request(baseUrl)
+	      .post('/people')
+	      .set('content-type', 'application/json')
+	      .send(JSON.stringify({people: [{ "name": "", "password": "qwerty", "email": "test@example.com"}]}))
+	      .end(function(error, response) {
+	      	response.body.should.have.keys(['error', 'detail']);
+	        response.body.detail.should.equal("Error: Validation error: Name can't be blank");
+	        done();
+	      });
+	  });
+
+		it('should receive an error response in correct format when PATCHing', function(done) {
+	    request(baseUrl)
+	      .patch('/people/' + ids.people[0])
+	      .set('content-type', 'application/json')
+	      .send(JSON.stringify([{ "path" : "/people/0/name", "op": "replace", "value": "Peter" }]))
+	      .end(function(error, response) {
+	      	response.body.should.have.keys(['error', 'detail']);
+	        response.body.detail.should.equal("Error: Validation error: Name 'Peter' is not allowed");
 	        done();
 	      });
 	  });
