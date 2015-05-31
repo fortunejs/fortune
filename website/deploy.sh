55d67d20724193db73c30f310f332c7642227a4d
Dali Zheng <dali@series.ac>
2015-05-31 02:26:04 +0200

    update deploy script

--- website/deploy.sh
+++ website/deploy.sh
@@ -7,8 +7,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 (cd "$DIR/../dist/web"; \
 	[ ! -d .git ] && \
 		git init && \
-		git remote add origin $REPO_GIT; \
-	git checkout -b gh-pages && \
+		git remote add origin $REPO_GIT &&
+		git checkout -b gh-pages; \
 	git add -A && \
 	git commit -m 'Update website from deploy script.' && \
 	git push -u origin gh-pages --force)
