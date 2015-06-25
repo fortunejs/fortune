5675d1a37d8c9a56018fb87c092703245a38ae13
kristof sajdak <kristof.sajdak@gmail.com>
2015-06-25 12:15:49 +0200

    using test database rather than testdb for consistency

--- test/config.js
+++ test/config.js
@@ -6,8 +6,8 @@ module.exports = {
         port: harvesterPort,
         options: {
             adapter: 'mongodb',
-            connectionString: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/testDB",
-            db: process.env.MONGODB || 'testDB',
+            connectionString: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/test",
+            db: process.env.MONGODB || 'test',
             inflect: true,
             oplogConnectionString: (process.env.OPLOG_MONGODB_URL || "mongodb://127.0.0.1:27017/local") + '?slaveOk=true'
         }
